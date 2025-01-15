3add8efc1d3434ee07b70f437f69f2c4139c75ac
bakukun <h1101201@gmail.com>
2025-01-15 15:18:04 +0900

    Spring Security #1day

--- filter-proxy/src/test/java/filter/FilterTest.java
+++ filter-proxy/src/test/java/filter/FilterTest.java
@@ -1,70 +1,52 @@
-package config.web;
+package filter;
 
-import java.util.Arrays;
-import java.util.List;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.FilterChainProxy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.util.AntPathMatcher;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import filter.SecurityFilterEx01;
-import filter.SecurityFilterEx02;
-import filter.SecurityFilterEx03;
-import filter.SecurityFilterEx04;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import config.AppConfig;
+import config.WebConfig;
 import jakarta.servlet.Filter;
-import jakarta.servlet.http.HttpServletRequest;
 
-@Configuration
-public class SecurityConfigEx01 {
-	@Bean
-	public FilterChainProxy securityFilterChainProxy() {
-		List<SecurityFilterChain> securityFileterChains = Arrays.asList(
-				new SecurityFilterChain() {
-					@Override
-					public boolean matches(HttpServletRequest request) {
-						String uri = request.getRequestURI().replaceAll(request.getContextPath(), "");
-						return new AntPathMatcher().match("/hello/**", uri);
-					}
-					@Override
-					public List<Filter> getFilters() {
-						return Arrays.asList(securityFilterEx01(), securityFilterEx02());
-					}
-				},
-				new SecurityFilterChain() {
-					@Override
-					public boolean matches(HttpServletRequest request) {
-						String uri = request.getRequestURI().replaceAll(request.getContextPath(), "");
-						return new AntPathMatcher().match("/ping/**", uri);
-					}
-					@Override
-					public List<Filter> getFilters() {
-						return Arrays.asList(securityFilterEx03(), securityFilterEx04());
-					}
-				}
-			);
-		
-		return new FilterChainProxy(securityFileterChains);
-	}
-	
-    @Bean
-    public SecurityFilterEx01 securityFilterEx01() {
-        return new SecurityFilterEx01();
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes={WebConfig.class, AppConfig.class})
+@WebAppConfiguration
+public class FilterTest {
+    private static MockMvc mvc;
+    
+    @BeforeAll
+    public static void setup(WebApplicationContext applicationContext) {
+    	Filter filter = (Filter)applicationContext.getBean("realFilter");
+    	
+    	mvc = MockMvcBuilders
+    			.webAppContextSetup(applicationContext)
+    			.addFilter(new DelegatingFilterProxy(filter) , "/*")
+    			.build();
+    }
+    
+    @Test
+    public void testHello() throws Throwable {
+    	mvc
+    		.perform(get("/hello"))
+    		.andExpect(status().isOk())
+    		.andExpect(cookie().value("RealFilter", "Works"))
+    		.andExpect(MockMvcResultMatchers.content().string("Hello World"))
+    		.andDo(MockMvcResultHandlers.print());
     }
 
-    @Bean
-    public SecurityFilterEx02 securityFilterEx02() {
-        return new SecurityFilterEx02();
-    }
-
-    @Bean
-    public SecurityFilterEx03 securityFilterEx03() {
-        return new SecurityFilterEx03();
-    }
-
-    @Bean
-    public SecurityFilterEx04 securityFilterEx04() {
-        return new SecurityFilterEx04();
-    }
+ 
 }
