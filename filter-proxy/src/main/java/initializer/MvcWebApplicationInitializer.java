c73bd7dc7aa61f6b78a1e92eef229dfd1faab562
bakukun <h1101201@gmail.com>
2025-01-15 15:15:16 +0900

    Spring Security #1day

--- filter-proxy/src/main/java/initializer/MvcWebApplicationInitializer.java
+++ filter-proxy/src/main/java/initializer/MvcWebApplicationInitializer.java
@@ -1,6 +1,10 @@
 package initializer;
 
 import config.WebConfig;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterRegistration.Dynamic;
+import jakarta.servlet.ServletContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -21,4 +25,9 @@ public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispat
     protected String[] getServletMappings() {
         return new String[] {"/"};
     }
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] { new DelegatingFilterProxy("realFilter") };
+	}
 }
--- filter-proxy/src/test/java/filter/FilterTest.java
+++ filter-proxy/src/test/java/filter/FilterTest.java
@@ -1,15 +1,70 @@
-package filter;
+package config.web;
 
-import config.WebConfig;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
+import java.util.Arrays;
+import java.util.List;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes={WebConfig.class})
-@WebAppConfiguration
-public class FilterTest {
-    private MockMvc mvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.AntPathMatcher;
+
+import filter.SecurityFilterEx01;
+import filter.SecurityFilterEx02;
+import filter.SecurityFilterEx03;
+import filter.SecurityFilterEx04;
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Configuration
+public class SecurityConfigEx01 {
+	@Bean
+	public FilterChainProxy securityFilterChainProxy() {
+		List<SecurityFilterChain> securityFileterChains = Arrays.asList(
+				new SecurityFilterChain() {
+					@Override
+					public boolean matches(HttpServletRequest request) {
+						String uri = request.getRequestURI().replaceAll(request.getContextPath(), "");
+						return new AntPathMatcher().match("/hello/**", uri);
+					}
+					@Override
+					public List<Filter> getFilters() {
+						return Arrays.asList(securityFilterEx01(), securityFilterEx02());
+					}
+				},
+				new SecurityFilterChain() {
+					@Override
+					public boolean matches(HttpServletRequest request) {
+						String uri = request.getRequestURI().replaceAll(request.getContextPath(), "");
+						return new AntPathMatcher().match("/ping/**", uri);
+					}
+					@Override
+					public List<Filter> getFilters() {
+						return Arrays.asList(securityFilterEx03(), securityFilterEx04());
+					}
+				}
+			);
+		
+		return new FilterChainProxy(securityFileterChains);
+	}
+	
+    @Bean
+    public SecurityFilterEx01 securityFilterEx01() {
+        return new SecurityFilterEx01();
+    }
+
+    @Bean
+    public SecurityFilterEx02 securityFilterEx02() {
+        return new SecurityFilterEx02();
+    }
+
+    @Bean
+    public SecurityFilterEx03 securityFilterEx03() {
+        return new SecurityFilterEx03();
+    }
+
+    @Bean
+    public SecurityFilterEx04 securityFilterEx04() {
+        return new SecurityFilterEx04();
+    }
 }
